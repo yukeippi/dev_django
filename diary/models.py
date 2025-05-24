c586f0e67710f8967a5a82b44a5e9a285b91f765
Yuichiro Kei <yuichirokei@gmail.com>
2025-05-24 01:34:41 +0000

    Brush up

--- diary/models.py
+++ diary/models.py
@@ -12,8 +12,7 @@ class DiaryEntry(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               default=1
+                               on_delete=models.CASCADE
                                )
     
     def __str__(self):
