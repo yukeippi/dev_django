477631d7bf9f810d997d5d82315b35e2d9bd1135
Yuichiro Kei <yuichirokei@gmail.com>
2025-05-27 09:03:39 +0000

    管理機能の追加

--- apps/todos/admin.py
+++ apps/todos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'created_at', 'completed_at']
+    list_filter = ['completed_at', 'created_at', 'user']
+    search_fields = ['title', 'description']
+    readonly_fields = ['created_at', 'updated_at']
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'user')
+        }),
+        ('日時情報', {
+            'fields': ('created_at', 'updated_at', 'completed_at'),
+            'classes': ('collapse',)
+        }),
+    )
