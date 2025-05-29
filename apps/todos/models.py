e93f79ebaa3d2f908a6d64bc64b42e1dd8f12014
Yuichiro Kei <yuichirokei@gmail.com>
2025-05-29 06:14:00 +0000

    編集機能の追加

--- apps/todos/models.py
+++ apps/todos/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from .validators import MinLengthExclusiveValidator, MaxLengthExclusiveValidator
 
 class Todo(models.Model):
     """Todoモデル"""
     
-    title = models.CharField('タイトル', max_length=200)
-    description = models.TextField('詳細', blank=True, null=True)
+    title = models.CharField(
+        'タイトル', 
+        max_length=200,
+        validators=[
+            MinLengthExclusiveValidator(10, message='Titleは10文字以上で入力してください。')
+        ]
+    )
+    description = models.TextField(
+        '詳細', 
+        max_length=500, 
+        blank=True, 
+        null=True,
+        validators=[
+            MaxLengthExclusiveValidator(10, message='descriptionは10文字未満で入力してください。')
+        ]
+    )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -20,6 +36,11 @@ class Todo(models.Model):
         verbose_name = 'Todo'
         verbose_name_plural = 'Todos'
         ordering = ['-created_at']  # 新しい順に並べる
+
+    def clean(self):
+        """モデルレベルでのバリデーション"""
+        super().clean()
+        # フォームレベルでバリデーションを行うため、ここでは基本的なcleanのみ実行
 
     def __str__(self):
         return self.title
--- apps/todos/views.py
+++ apps/todos/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from django.views import View
+from django.contrib import messages
+from django.core.exceptions import ValidationError
 
 from apps.todos.models import Todo
+from apps.todos.forms import TodoForm
 
 class TodoListView(View):
     '''Todo一覧'''
@@ -13,20 +16,48 @@ class TodoListView(View):
 class TodoDetailView(View):
     '''Todo詳細'''
     def get(self, request, pk):
-        todo = Todo.objects.get(pk=pk)
+        todo = get_object_or_404(Todo, pk=pk)
         return render(request, 'todos/detail.html', {'todo': todo})
 
 class TodoEditView(View):
+    '''編集画面'''
     def get(self, request, pk):
-        todo = Todo.objects.get(pk=pk)
-        return render(request, 'todos/edit.html', {'form': todo})
+        todo = get_object_or_404(Todo, pk=pk)
+        form = TodoForm(instance=todo)
+        return render(request, 'todos/edit.html', {
+            'form': form,
+            'todo': todo
+        })
 
+    '''更新処理'''
     def post(self, request, pk):
-        todo = Todo.objects.get(pk=pk)
-        todo.title = request.POST.get('title')
-        todo.description = request.POST.get('description')
-        todo.save()
-        return render(request, 'todos/detail.html', {'todo': todo})
+        todo = get_object_or_404(Todo, pk=pk)
+        form = TodoForm(request.POST, instance=todo)
+        
+        if form.is_valid():
+            try:
+                # モデルのcleanメソッドを呼び出してバリデーション
+                todo_instance = form.save(commit=False)
+                todo_instance.full_clean()
+                todo_instance.save()
+                messages.success(request, 'Todoが正常に更新されました。')
+                return redirect('todos:todos_detail', pk=todo.pk)
+            except ValidationError as e:
+                # モデルレベルのバリデーションエラーをフォームに追加
+                for field, errors in e.message_dict.items():
+                    for error in errors:
+                        form.add_error(field, error)
+                messages.error(request, 'フォームにエラーがあります。')
+                return render(request, 'todos/edit.html', {
+                    'form': form,
+                    'todo': todo
+                })
+        else:
+            messages.error(request, 'フォームにエラーがあります。')
+            return render(request, 'todos/edit.html', {
+                'form': form,
+                'todo': todo
+            })
 
 list_view = TodoListView.as_view()
 detail_view = TodoDetailView.as_view()
