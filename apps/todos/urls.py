f44fc9ebdcae1c31bf2cb20c09042e5694931f27
Yuichiro Kei <yuichirokei@gmail.com>
2025-05-29 07:27:35 +0000

    追加機能の追加

--- apps/todos/urls.py
+++ apps/todos/urls.py
@@ -6,5 +6,6 @@ app_name = 'todos'
 urlpatterns = [
     path('', views.list_view, name='todos_list'),
     path('<int:pk>/', views.detail_view, name='todos_detail'),
+    path('create/', views.create_view, name='todos_create'),
     path('edit/<int:pk>/', views.edit_view, name='todos_edit'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,8 @@ if DEBUG:
     import socket
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS += [ip[:-1] + '1' for ip in ips]
+
+# Authentication settings
+LOGIN_URL = '/admin/login/'
+LOGIN_REDIRECT_URL = '/todos/'
+LOGOUT_REDIRECT_URL = '/'
